a6cd3219d84861415f71ab3e2ffe2193ea498bfa
jihyun <noblea1117@gmail.com>
2015-10-20 23:32:23 +0900

    add chapter5 comprehension

--- python/tutorial/ch5.py
+++ python/tutorial/ch5.py
@@ -54,15 +54,15 @@ print(queue)
 
 
 """
-# TODO
+append 함수를 통해 list에 값을 계속 추가할 수 있다.
 """
 squares = []
 for x in range(10):
     squares.append(x**2)
 print(squares)
 
-squares = list(map(lambda x: x**2, range(10)))
-squares = [x**2 for x in range(10)]
+squares = list(map(lambda x: x**2, range(10))) # 위의 loop 돌아서 집어넣는 것과 동일한 value
+squares = [x**2 for x in range(10)] # 위와 동일
 
 [(x, y) for x in [1, 2, 3] for y in [3, 1, 4] if x != y]
 combs = []
@@ -74,7 +74,7 @@ print(combs)
 
 
 """
-# TODO
+list를 이용하여 아래와 같은 다양한 시도들을 진행할 수 있다.
 """
 vec = [-4, -2, 0, 2, 4]
 [x*2 for x in vec]
@@ -89,7 +89,7 @@ vec = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
 
 """
-# TODO
+list를 중첩하여 사용할 수 있다.
 """
 matrix = [
     [1, 2, 3, 4],
@@ -105,10 +105,6 @@ for i in range(4):
 
 print(transposed)
 
-
-"""
-# TODO
-"""
 transposed = []
 for i in range(4):
     transposed_row = []
@@ -120,11 +116,17 @@ print(transposed)
 
 
 """
-# TODO
+zip 함수는 matrix에 있는 원소들의 각 행들의 첫번째 값, 두번째 값... 을 같은 리스트로 묶어서 표현해준다.
 """
 print(list(zip(*matrix)))
 
 
+"""
+리스트에서 del 함수를 이용해 값을 지우는 예제
+del b[0]을 수행하면 리스트의 첫번째 값이 지워지고, 
+b[2:4]를 수행하면 리스트에서 두번째 값부터 세 번째 값까지의 값이 지워진다.
+b[:] 는 리스트 전체를 의미하며 del b[:] 수행 시 리스트 b에 있는 값 전체 삭제
+"""
 b = [-1, 1, 66.25, 333, 333, 1234.5]
 del b[0]
 print(b)
@@ -137,23 +139,39 @@ print(b)
 
 
 """
-# TODO
+파이썬에는 tuple이라는 자료구조가 존재한다.
+선언은 t = 1234, 234, 'abc' 이런 방식으로 진행
+tuple 값 호출은 t[해당 인덱스] 방식으로 호출
 """
 t = 12345, 54321, 'hello!'
 print(t[0])
 print(t)
 
+"""
+tuple 간에 중첩도 가능하다.
+"""
 u = t, (1, 2, 3, 4, 5)
 print(u)
 
+"""
+tuple은 고정값이기 때문에 t[0]에 새로운 값을 집어넣는 것이 허용되지 않는다.
+"""
 #t[0] = 88888
 
+"""
+tuple에 list도 집어넣을 수 있다.
+"""
 v = ([1, 2, 3], [3, 2, 1])
 print(v)
 
 
 """
-# TODO
+tuple과 list의 차이는, tuple의 경우 변하지 않고, 다양한 자료형이 들어간다.
+list는 종종 변하고, 하나의 자료형으로만 구성되어 있다.
+
+빈 tuple을 선언하고 싶을 때는 empty = () 와 같이 선언해준다.
+sigleton = 'hello'로 선언했을 때는 sigleton을 string을 담은 변수로 간주하지만,
+sigleton = 'hello', 라 선언하면 tuple로 간주한다.
 """
 empty = ()
 sigleton = 'hello',
@@ -165,26 +183,28 @@ x, y, z = t
 
 
 """
-# TODO
+파이썬에는 집합 자료구조도 존재한다. 
+집합은 중복이 허용되지 않으며 우리가 알고 있는 집합 연산 모든 것을 허용한다.
 """
 basket = {'apple', 'orange', 'apple', 'pear', 'orange', 'banana'}
 print(basket)
-'orange' in basket
-'crabgrass' in basket
+'orange' in basket # result : True
+'crabgrass' in basket # result : False
 
 a = set('abracadabra')
 b = set('alacazam')
-print(a)
-print(a-b)
-print(a&b)
-print(a^b)
+print(a) # 각각 문자를 하나의 원소로 간주, 동일한 값이 여러번 들어왔을 때 하나로 간주
+print(a-b) # 차집합
+print(a&b) # 합집합
+print(a^b) # a나 b에 포함되어 있으나 공통으로 포함되어 있지는 않은 원소들
 
 b = {x for x in 'abracadabra' if x not in 'abc'}
-print(b)
+print(b) # a, b, c는 공통으로 포함되어 있으므로 d, r 출력
 
 
 """
-# TODO
+Dictionary에 대한 자료구조
+key, value로 구성되어 있다.
 """
 tel = {'jack': 4098, 'sape': 4139}
 tel['guido'] = 4127
@@ -198,20 +218,20 @@ print(sorted(tel.keys()))
 print('guido' in tel)
 print('jack' not in tel)
 
-
-"""
-# TODO
-"""
 dict([('sape', 4139), ('guido', 4127), ('jack', 4098)])
 {x: x**2 for x in (2, 4, 6)}
 dict(sape=4139, guido=4127, jack=4098)
 
+
+"""
+looping을 하는 여러 가지 방법들
+"""
 knights = {'gallahad': 'the pure', 'robin': 'the brave'}
 for k, v in knights.items():
-    print(k, v)
+    print(k, v) # key, value값이 차례대로 출력
 
 for i, v in enumerate(['tic', 'tac', 'toe']):
-    print(i, v)
+    print(i, v) # 0 tic ....
 
 questions = ['name', 'quest', 'favorite color']
 answers = ['lancelot', 'the holy grail', 'blue']
@@ -221,17 +241,13 @@ for q, a in zip(questions, answers):
 for i in reversed(range(1, 10, 2)):
     print(i)
 
-
-"""
-# TODO
-"""
 basket = ['apple', 'orange', 'apple', 'pear', 'orange', 'banana']
 for f in sorted(set(basket)):
     print(f)
 
 
 """
-# TODO
+isnan 함수를 통해 값의 NaN 여부를 확인할 수 있음
 """
 import math
 raw_data = [56.2, float('NaN'), 51.7, 55.3, 52.5, float('NaN'), 47.8]
@@ -244,7 +260,7 @@ print(filtered_data)
 
 
 """
-# TODO
+python에서 연산은 nested 될 수 있다.
 """
 string1, string2, string3 = '', 'Trondheim', 'Hammer Dance'
 non_null = string1 or string2 or string3
@@ -252,7 +268,7 @@ print(non_null)
 
 
 """
-# TODO
+여러 타입과 값을 비교할 수 있다.
 """
 (1, 2, 3) < (1, 2, 4)
 [1, 2, 3] < [1, 2, 4]
